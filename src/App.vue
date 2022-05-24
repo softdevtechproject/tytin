9b40eb0273eac2f989e283f10f01af499066f731
softdevtechproject <104557955+softdevtechproject@users.noreply.github.com>
2022-05-24 10:46:17 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import Main from "@/views/MainView.vue";
+</script>
+
+<template>
+  <header>
+    <nav>
+      <div class="topnav">
+        <RouterLink to="/" id="compname" class="nav__link">TYTIN</RouterLink>
+        <RouterLink to="/" class="nav__link">Home</RouterLink>
+        <RouterLink to="/menu" class="nav__link">Menu</RouterLink>
+        <RouterLink to="/about" class="nav__link">About us</RouterLink>
+        <RouterLink to="/contacts" class="nav__link">Contacts</RouterLink>
+      </div>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+@import "@/assets/base.css";
+</style>
